00c03b46c1f39864540280ce6ce0d46b39e40457
fcdev <fedechico1@live.it>
2020-01-29 23:16:17 +0100

    Last commit with junctions (they suck)

---
+++ Assets/Scripts/Dialogue/Nodes/Event.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using Unity;
+using UnityEngine.Events;
+
+namespace Dialogue.Functional
+{
+	[NodeTint("#FFFFAA")]
+	public class Event : DialogueBaseNode {
+        [Input] public Connection input;
+        [Output] public Connection output;
+
+        public UnityEvent[] trigger;
+
+		public override void Trigger() {
+			for (int i = 0; i < trigger.Length; i++) {
+				trigger[i].Invoke();
+			}
+            goToOutput();
+        }
+
+	}
+}
