528663594b4cb152c359f03edad24e1bcb67dad3
Federico Capece <fedechico1@live.it>
2020-02-01 20:50:26 +0100

    PolloManager should be ok, still no bomb script

--- Assets/PolloManager.cs
+++ Assets/PolloManager.cs
@@ -7,6 +7,8 @@ public class PolloManager : MonoBehaviour
     public float nextChickenCheck = 0f;
     public float chickenCheckTimer = 1f;
 
+    public GameObject[] destroyOnClear;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,9 +18,16 @@ public class PolloManager : MonoBehaviour
 
             bool rotationOk = true;
 
-            for (int i = 0; i < transform.childCount && rotationOk; i++) {
-                if (transform.GetChild(i)) { 
-                
+            for (int i = 0; i < transform.childCount; i++) {
+                if (transform.GetChild(i).rotation.eulerAngles.x != 0) {
+                    rotationOk = false;
+                    break;
+                }
+            }
+
+            if (rotationOk) {
+                foreach (GameObject toDestroy in destroyOnClear) {
+                    Destroy(toDestroy);
                 }
             }
 
