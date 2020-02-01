bd0d05e5833418814bdd537a4f58d3c054952450
Federico Capece <fedechico1@live.it>
2020-02-01 09:10:14 +0100

    canvas <3

---
+++ Assets/TextScaler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class TextScaler : MonoBehaviour
+{
+    public int referenceWidth = 384;
+    public int pixelSize = 8;
+
+    private void Start() {
+        GetComponent<Text>().fontSize = Screen.width / referenceWidth * pixelSize;
+        GetComponent<Text>().font.material.mainTexture.filterMode = FilterMode.Point;
+    }
+}
