5b2ec255296c6a5524d351acdd6442387c1f7082
Federico Capece <fedechico1@live.it>
2020-02-01 10:37:42 +0100

    bug prefab

---
+++ Assets/Bug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : Killable
+{
+    public float seedChange = 0;
+
+    public float minSeedTime = .3f;
+    public float maxSeedTime = 2f;
+
+    public float minSeed = 1;
+    public float maxSeed = 4;
+
+
+    private void Start() {
+        Random.InitState ((int)(Time.time * Time.deltaTime * 1000));
+        seedChange = Time.time + Random.Range(minSeedTime, maxSeedTime);
+    }
+
+}
