b68bdff83139fa0426eb920c66bcb176941fb362
Federico Capece <fedechico1@live.it>
2020-02-02 12:51:26 +0100

    pollo_sound ok

---
+++ Assets/PolloBarrier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolloBarrier : Killable
+{
+    public PolloSpawner spawn;
+    protected override bool checkHit(Collider2D collision) {
+        hp++;
+        bool hit = base.checkHit(collision);
+        if (hit) {
+            spawn.enabled = true;
+        }
+        return hit;
+    }
+}
---
+++ Assets/PolloSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolloSpawner : MonoBehaviour
+{
+    public GameObject polloPrefab;
+
+    public float pollosPerSecond = 1;
+
+    public float nextPolloSpawnTime;
+
+    public float speedVariability = 2;
+    public float pitchVariability = 1.5f;
+
+    public void Update() {
+        if (Time.time > nextPolloSpawnTime) {
+            //setting the timer again
+            nextPolloSpawnTime = Time.time + 1f / pollosPerSecond;
+
+            //picking a random spawnpoint for the cucco
+            Vector3 spawn = transform.GetChild(Random.Range(0, transform.childCount)).position;
+
+            //spawning the cucco
+            GameObject cucco = GameObject.Instantiate(polloPrefab, spawn, Quaternion.identity);
+            Vector3 direction = Player.gameObject.transform.position - spawn;
+            cucco.GetComponent<Projectile>().direction = direction.normalized;
+
+            //variating speed
+            cucco.GetComponent<Projectile>().speed += Random.Range(-speedVariability, speedVariability);
+
+            //variating pitch
+            cucco.GetComponent<AudioSource>().pitch += Random.Range(-pitchVariability, pitchVariability);
+
+            //flipping it in case it's not facing the player
+            if (direction.x > 0) {
+                cucco.transform.localScale = new Vector3(-1, 1, 1);
+            }
+
+        }
+    }
+}
